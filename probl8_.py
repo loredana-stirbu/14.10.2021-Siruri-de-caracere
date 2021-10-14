ce8894e227750710cba843a7934de293caa41c3a
Loredana <70909576+loredana-stirbu@users.noreply.github.com>
2021-10-14 18:45:27 +0300

    Update probl8_.py

--- probl8_.py
+++ probl8_.py
@@ -1,24 +1,23 @@
-lst="A B C D E F G H I J K L M N O P Q R S T U V W X Y"
+lst=str(input("Scrie un sir"))
 l1=""
 l2=""
 l3=""
+print("Sirul initial: ", lst)
 
 for i in lst:
-    x=chr(ord(i)+1)
-    l1+=x
-    b=l1.replace('!',' ')
-    s=b.replace('[','A') #am folosit acest replace deoarece in cazul cind vom avea insusi Z in sir(daca creem un sir nou), acesta va fi inlocuit cu urmtorul caracter(el fiind"]"), pe cind noua near trebui sa se reia ciclul de la prima litera din alfabet"
-print("Sirul1:", s)
+    if ((ord(i)>64) and (ord(i)<90)):
+        x=chr(ord(i)+1)
+        l1+=x
+    else:
+        l1+=i      
+print("Sirul1:", l1)
 
-l2=s
+l2=l1
 for i in lst:
     w=l2.replace(("Z"), ("A"))
-    c=w.replace('!',' ')
-    k=c.replace('[','A')
-print("Sirul2:", k) 
+print("Sirul2:", w) 
 
-l3=k
+l3=w
 for i in lst:
     f=l3.replace(' ','-')
-    b=f.replace('[','A')
-print("Sirul3:", b)
+print("Sirul3:", f)
